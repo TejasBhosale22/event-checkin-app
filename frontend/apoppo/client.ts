3f3742a1ae6da525463839d38e2a1610cc79a5db
Tejas Bhosale <bhosale22tejas@gmail.com>
2025-06-26 14:57:11 +0530

    Update client.ts

--- frontend/apoppo/client.ts
+++ frontend/apoppo/client.ts
@@ -1,4 +1,3 @@
-// apollo/client.ts
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { useAuthStore } from '../src/store/useAuthStore';
