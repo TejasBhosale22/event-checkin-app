46181a85362a5edadb1eece943ea8e5aeb6eb2c4
Tejas Bhosale <bhosale22tejas@gmail.com>
2025-06-26 14:14:47 +0530

    Add files via upload

---
+++ frontend/apoppo/client.ts
@@ -0,0 +1,23 @@
+// apollo/client.ts
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { useAuthStore } from '../src/store/useAuthStore';
+
+const httpLink = createHttpLink({
+  uri: 'http://192.168.29.224:4000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = useAuthStore.getState().token;
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
